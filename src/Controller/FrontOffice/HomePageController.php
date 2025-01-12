857271f33f79dc4e8cb4268596303eda7da4fbab
Somenae <kevindevaux@neuf.fr>
2025-01-12 19:02:07 +0100

    one line removed

--- src/Controller/FrontOffice/HomePageController.php
+++ src/Controller/FrontOffice/HomePageController.php
@@ -4,7 +4,6 @@ namespace App\Controller\FrontOffice;
 
 use App\Repository\ProductRepository;
 use App\Service\FrontOffice\CategoryService;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
